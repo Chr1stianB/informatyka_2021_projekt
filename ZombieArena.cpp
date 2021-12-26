f71cb101210f377eafb0c29cf64d7cd210208f67
Chris <krybrzo13@gmail.com>
2021-12-26 10:59:16 +0100

    Początek gry

--- ZombieArena.cpp
+++ ZombieArena.cpp
@@ -55,8 +55,144 @@ int main()
 	// Glowna petla
 	while (window.isOpen())
 	{
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::KeyPressed)
+			{
+				// Zapauzuj gre jezli w trakcie
+				if (event.key.code == Keyboard::Return &&
+					state == State::PLAYING)
+				{
+					state = State::PAUSED;
+				}
+				// zrestartuj jezeli zapauzowana
+				else if (event.key.code == Keyboard::Return &&
+					state == State::PAUSED)
+				{
+					state = State::PLAYING;
+					// Resetowanie zegara
+					clock.restart();
+				}
+				// Rozpicznij nowa gre jezeli w trybie GAME_OVER
+				else if (event.key.code == Keyboard::Return && state == State::GAME_OVER)
+				{
+					state = State::LEVELING_UP;
+				}
+				if (state == State::PLAYING)
+				{
+					;
+				}
+			}
+		} // Koniec event pooling
+		// Gracz wychodzi po wcisnieciu ESC
+		if (Keyboard::isKeyPressed(Keyboard::Escape))
+		{
+			window.close();
+		}
+		// Kontrolowanie ruchu z klawiatury
+		if (state == State::PLAYING)
+		{
+			if (Keyboard::isKeyPressed(Keyboard::W))
+			{
+				player.moveUp();
+			}
+			else
+			{
+				player.stopUp();
+			}
+			if (Keyboard::isKeyPressed(Keyboard::S))
+			{
+				player.moveDown();
+			}
+			else
+			{
+				player.stopDown();
+			}
+			if (Keyboard::isKeyPressed(Keyboard::A))
+			{
+				player.moveLeft();
+			}
+			else
+			{
+				player.stopLeft();
+			}
+			if (Keyboard::isKeyPressed(Keyboard::D))
+			{
+				player.moveRight();
+			}
+			else
+			{
+				player.stopRight();
+			}
+		} // Zakonczenie instrukcji sterujacych WASD
+		// Stan LEVELING_UP
+		if (state == State::LEVELING_UP)
+		{
+			if (event.key.code == Keyboard::Num1)
+			{
+				state = State::PLAYING;
+			}
+			if (event.key.code == Keyboard::Num2)
+			{
+				state = State::PLAYING;
+			}
+			if (event.key.code == Keyboard::Num3)
+			{
+				state = State::PLAYING;
+			}
+			if (event.key.code == Keyboard::Num4)
+			{
+				state = State::PLAYING;
+			}
+			if (event.key.code == Keyboard::Num5)
+			{
+				state = State::PLAYING;
+			}
+			if (event.key.code == Keyboard::Num6)
+			{
+				state = State::PLAYING;
+			}
+			if (state == State::PLAYING)
+			{
+				// Przygotowanie poziomu 
+				arena.width = 500;
+				arena.height = 500;
+				arena.left = 0;
+				arena.top = 0;
 
-	}
+				int tileSize = 50;
+				player.spawn(arena, resolution, tileSize);
+				clock.restart();
+			}
+		} // Koniec PETLI LEVELING_UP
+		if (state == State::PLAYING)
+		{
+			// Aktualizajca czasu
+			Time dt = clock.restart();
+
+			// Aktualizacja calkowiteg czasu gry
+			gameTimeTotal += dt;
+
+			// Zaokraglij czas gry
+			float dtAsSeconds = dt.asSeconds();
+
+			// Gdzie jest wskaznik myszy
+			mouseScreenPosition = Mouse::getPosition();
+
+			// Konwertuj polozenie myszy na koorydnaty swiata z mainView
+			mouseWorldPosition = window.mapPixelToCoords(Mouse::getPosition(), mainView);
+
+			// Aktualizuj gracza
+			player.update(dtAsSeconds, Mouse::getPosition());
+
+			// Zapisz nowa pozycje gracza
+			Vector2f playerPosition(player.getCenter());
+
+			// Wysrodkuj View dookola gracza
+			mainView.setCenter(player.getCenter());
+		}// Koniec updaetowania sceny
+	} // Koniec glownej petli
 
 	return 0;
-} //255
+} 
